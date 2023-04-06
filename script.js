38fcd53b73937d3d1228d2c12f2aa18a3934e225
Oluwatobi <oluwatobiogungbangbe@gmail.com>
2023-04-06 17:50:24 +0100

    building front interface

---
+++ script.js
@@ -0,0 +1,20 @@
+const questions = [
+    {
+        question: "In which part of your body would you find the cruciate ligament?",
+        answers: [
+            { text: "Neck", correct: false},
+            { text: "Brain", correct: false},
+            { text: "Knee", correct: true},
+            { text: "Head", correct: false},
+        ]
+    },
+    {
+        question: "Who was the head of state in Japan during the second world war?",
+        answers: [
+            { text: "Emperor Hirohito", correct: true},
+            { text: "General Hana", correct: false},
+            { text: "Emperor Hiroshi", correct: true},
+            { text: "Prime minister Emiko", correct: false},
+        ]
+    }
+]
